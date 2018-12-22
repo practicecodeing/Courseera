d54af3fd3a775d25118fd08ea9a4871648ac78e2
Subramanyam <subramanyam.p@aavidmanagement.com>
2018-12-22 21:59:31 +0530

    code formating

--- coursera-angular-assignment1/app.js
+++ coursera-angular-assignment1/app.js
@@ -5,28 +5,25 @@
   .controller('myController', function($scope){
       $scope.items = "";
       $scope.totalitemcount = 0;
-$scope.report = "";
-
-$scope.checkIfHealthy = function(){
-  var count = getItemsCount($scope.items);
-  $scope.totalitemcount = count;
-  var reportdata = "";
-    if (count== 0) {
-      reportdata = "Please enter data first";
-    }else if(count>0 && count<=3) {
-reportdata = "Enjoy!";
-}else if(count>3) {
-reportdata = "Too much!";
-    }
-    $scope.report = reportdata;
-};
-
-function  getItemsCount(itemList){
-var count = 0;
-if (itemList.length>0)
-  count = itemList.split(',').length;
-
-return count;
-}
+      $scope.report = "";
+      $scope.checkIfHealthy = function(){
+        var count = getItemsCount($scope.items);
+        $scope.totalitemcount = count;
+        var reportdata = "";
+          if (count== 0) {
+            reportdata = "Please enter data first";
+          }else if(count>0 && count<=3) {
+            reportdata = "Enjoy!";
+          }else if(count>3) {
+            reportdata = "Too much!";
+          }
+          $scope.report = reportdata;
+      };
+      function  getItemsCount(itemList){
+        var count = 0;
+        if (itemList.length>0)
+          count = itemList.split(',').length;
+        return count;
+      }
   });
 })(); /// iife end
